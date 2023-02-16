1d69673f2da0cec1439e08e35e08e1967b33c052
dapereir <dapereir@student.42lyon.fr>
2023-02-16 16:54:25 +0100

    fix: use 2 child processes

--- src/pip_pipe.c
+++ src/pip_pipe.c
@@ -6,13 +6,13 @@
 /*   By: dapereir <dapereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 11:25:40 by dapereir          #+#    #+#             */
-/*   Updated: 2023/02/16 16:03:15 by dapereir         ###   ########.fr       */
+/*   Updated: 2023/02/16 16:54:03 by dapereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	pip_child_process(t_pip *pip, int level)
+static void	pip_child_process_1(t_pip *pip, int level)
 {
 	int		*fd_pipe;
 	int		fd_in;
@@ -35,14 +35,12 @@ static void	pip_child_process(t_pip *pip, int level)
 	pip_execute(pip, pip->cmd[level], pip->envp);
 }
 
-static void	pip_parent_process(t_pip *pip, int level)
+static void	pip_child_process_2(t_pip *pip, int level)
 {
-	int	status;
 	int	*fd_pipe;
 
 	fd_pipe = pip->fd_pipe + 2 * level;
 	close(fd_pipe[1]);
-	waitpid(-1, &status, 0);
 	if (pip->cmd_size - level > 2)
 		pip_pipe(pip, level + 1);
 	else if (pip->cmd_size - level == 2)
@@ -56,17 +54,26 @@ static void	pip_parent_process(t_pip *pip, int level)
 
 void	pip_pipe(t_pip *pip, int level)
 {
-	pid_t	cpid;
+	pid_t	cpid_1;
+	pid_t	cpid_2;
 	int		*fd_pipe;
+	int		status;
 
 	fd_pipe = pip->fd_pipe + 2 * level;
 	if (pipe(fd_pipe) == -1)
 		pip_error_exit(pip, "Pipe failed");
-	cpid = fork();
-	if (cpid == -1)
+	cpid_1 = fork();
+	if (cpid_1 == -1)
 		pip_error_exit(pip, "Fork failed");
-	if (cpid == 0)
-		pip_child_process(pip, level);
-	else
-		pip_parent_process(pip, level);
+	if (cpid_1 == 0)
+		pip_child_process_1(pip, level);
+	cpid_2 = fork();
+	if (cpid_2 == -1)
+		pip_error_exit(pip, "Fork failed");
+	if (cpid_2 == 0)
+		pip_child_process_2(pip, level);
+	close(fd_pipe[0]);
+	close(fd_pipe[1]);
+	waitpid(cpid_1, &status, 0);
+	waitpid(cpid_2, &status, 0);
 }
